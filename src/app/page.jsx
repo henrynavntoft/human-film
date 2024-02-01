0db25305b8eec9afcd8296a9cfb833a4e520d17c
Henry Navntoft <henrylnavntoft@gmail.com>
2024-02-01 14:01:15 +0100

    window update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,34 +4,42 @@ import { useEffect, useRef, useState } from "react";
 
 const BackgroundVideo = () => {
   const videoRef = useRef(null);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(null);
 
   useEffect(() => {
+    // Set the initial width
+    setWindowWidth(window.innerWidth);
+
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
     window.addEventListener("resize", handleResize);
 
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
     const video = videoRef.current;
     if (video) {
       video.play().catch((error) => console.error("Video play failed:", error));
     }
-
-    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   let videoSource, videoClass;
   if (windowWidth < 600) {
     videoSource = "/mobile-video.mp4";
-    videoClass = "object-cover h-full w-full"; // Adjust as needed for vertical video
-  } else if (windowWidth < 1000) {
+    videoClass = "object-cover w-full h-full"; // Adjust as needed for vertical video
+  } else if (windowWidth < 1200) {
     videoSource = "/tablet-video.mp4";
     videoClass = "w-full h-full object-cover";
   } else {
     videoSource = "/video.mp4";
     videoClass = "w-full h-full object-cover";
   }
+
+  // Return null if windowWidth is not set yet
+  if (windowWidth === null) return null;
 
   return (
     <div className="fixed inset-0 w-full h-full z-0 overflow-hidden">
