a47f97e4b0b1ccc8c07bd5b4e22f7f3121b0bfa4
Henry Navntoft <henrylnavntoft@gmail.com>
2024-02-01 14:07:16 +0100

    center logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,12 +5,12 @@ export default function Header() {
   return (
     // Ensure the header has a higher z-index value to appear on top of the background video.
     // Adding 'bg-opacity-75 bg-black' for better visibility over the video if needed.
-    <div className="flex justify-between items-center w-full p-4 z-50 fixed top-0 left-0 right-0 bg-opacity-75 text-white">
+    <div className="flex justify-between items-center w-full py-4 sm:px-12 px-4  z-50 fixed top-0 left-0 right-0 bg-opacity-75 text-white">
       <Link href="/people">
         <p className="">PEOPLE</p>
       </Link>
 
-      <Link href="/">
+      <Link className="ml-4" href="/">
         {/* <p className="text-2xl">
           <span className="logo-human">HUMAN</span>FILM
           <span className="logo-icon">&#174;</span>
