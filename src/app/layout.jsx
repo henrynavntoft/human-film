830bfccf4122946c9700197788f762af085decf4
Henry Navntoft <henrylnavntoft@gmail.com>
2024-02-16 12:58:51 +0100

    added favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/human.favicon.svg" sizes="any" />
         <link
           rel="stylesheet"
           href="https://use.typekit.net/jrh2pge.css"
